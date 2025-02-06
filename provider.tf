2b1181f8a2b3a0480470ff3c6cc5859bfc9a8413
Nihalkumar231996 <125646865+Nihalkumar231996@users.noreply.github.com>
2025-02-06 21:50:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "devops-shack-s3"
+    bucket         = "bucketforeksterraform"
     key            = "eks.terraform.tfstate"
     region         = "ap-south-1"
     
