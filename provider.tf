4a2e8927d9086b5d30e6fe970f1a05d186335f10
Nihalkumar231996 <125646865+Nihalkumar231996@users.noreply.github.com>
2025-02-12 15:43:33 +0530

    Update bucket name provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "mybucket12feb"
+    bucket         = "terraformbucker12feb"
     key            = "eks.terraform.tfstate"
     region         = "ap-south-1"
     
